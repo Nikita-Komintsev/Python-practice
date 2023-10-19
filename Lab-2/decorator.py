20d4b19c6b14a3c93fbe8397b45a5e830388effc
Nikita-Komintsev <nikita.comin@gmail.com>
2023-10-19 22:43:48 +0300

    finish decorator.py

--- Lab-2/decorator.py
+++ Lab-2/decorator.py
@@ -10,15 +10,15 @@ class Decorator:
     def __name__(self):
         return self.function.__name__
 
-    def log(self, *args, **kwargs):
+    def log(self, *args):
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
         info = f"{current_time}: function {self.function.__name__} called with arguments {args}"
         self.call_history.append(info)
-        #print(f"\n<{current_time}>: function <{self.function.__name__}> called with arguments <{args}>")
 
 
-class TimerDecorator(Decorator):
+
+class TimingDecorator(Decorator):
     @property
     def __name__(self):
         return self.function.__name__
@@ -28,25 +28,25 @@ class TimerDecorator(Decorator):
         result = self.function(*args, **kwargs)
         self.finish = time.perf_counter() - start
 
-        self.log(*args, **kwargs)
+        self.log(*args)
 
         print(f"\n{self.finish:.4f} in {__class__.__name__}")
         return result
 
 
-class HtmlOutputDecorator(Decorator):
+class HtmlTimingDecorator(Decorator):
     def __call__(self, *args, **kwargs):
         result = self.function(*args, **kwargs)
 
-        self.log(*args, **kwargs)
+        self.log(*args)
 
         print(f"<html><body>{self.function.finish:.4f}</body></html> in {__class__.__name__}")
 
         return result
 
 
-@HtmlOutputDecorator
-@TimerDecorator
+@HtmlTimingDecorator
+@TimingDecorator
 def square_for(nums):
     result = []
     for num in nums:
@@ -54,14 +54,14 @@ def square_for(nums):
     return result
 
 
-@HtmlOutputDecorator
-@TimerDecorator
+@HtmlTimingDecorator
+@TimingDecorator
 def square_list_comprehension(nums):
     return [num ** 2 for num in nums]
 
 
-@HtmlOutputDecorator
-@TimerDecorator
+@HtmlTimingDecorator
+@TimingDecorator
 def square_map(nums):
     return list(map(lambda x: x ** 2, nums))
 
@@ -73,15 +73,6 @@ square_list_comprehension(numbers)
 square_map(numbers)
 
 print("\nHistory:")
-# for call_info in square_for.call_history:
-#     print(call_info)
-#
-# for call_info in square_list_comprehension.call_history:
-#     print(call_info)
-#
-# for call_info in square_map.call_history:
-#     print(call_info)
-
 for func_decorator in [square_for, square_list_comprehension, square_map]:
     for call_info in func_decorator.call_history:
         print(call_info)
