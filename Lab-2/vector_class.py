7ec71f3cf1881074e0c85d40068903d9ea9b0da5
Nikita-Komintsev <nikita.comin@gmail.com>
2023-11-05 12:58:41 +0300

    fix vector_class.py

--- Lab-2/vector_class.py
+++ Lab-2/vector_class.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Vector:
     def __init__(self, x, y):
         self.x = x
@@ -16,20 +19,28 @@ class Vector:
         return not self.__eq__(other)
 
     def __mul__(self, scalar):
-        return Vector(self.x * scalar, self.y * scalar)
+        if (type(scalar) is int) or (type(scalar) is float):
+            return Vector(self.x * scalar, self.y * scalar)
+        elif type(scalar) is Vector:
+            return self.x * scalar.x + self.y * scalar.y
+        else:
+            raise Exception("Unknown argument")
 
-    def scalar_product(self, other):
-        return self.x * other.x + self.y * other.y
+    def __rmul__(self, scalar):
+        return self * scalar
 
     def __len__(self):
         return int((self.x ** 2 + self.y ** 2) ** 0.5)
+
+    def __abs__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def __str__(self):
         return f"<{self.x}; {self.y}>"
 
 
 v1 = Vector(1, 2)
-v2 = Vector(3, 4)
+v2 = Vector(3.1, 4)
 
 print(f'V1 = {v1}')
 print(f'V2 = {v2}')
@@ -38,5 +49,6 @@ print(f'Сложение: {v1 + v2}')
 print(f'Вычитание: {v1 - v2}')
 print(f'Сравнение: {v1 == v2}  {v1 != v2}')
 print(f'Умножение на число: {v1 * 5}')
-print(f'Скалярное произведение: {v1.scalar_product(v2)}')
-print(f'Длина v2: {len(v2)}')
+print(f'Умножение числа на вектор: {7 * v1}')
+print(f'Скалярное произведение: {v1*v2}')
+print(f'Длина v2: {abs(v2)}')
