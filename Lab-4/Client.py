cbcab3d7039a209c35a7d4c75333221c3f94d347
nikita <nikita.comin@gmail.com>
2023-12-18 12:20:06 +0300

    save in docx

--- Lab-4/Client.py
+++ Lab-4/Client.py
@@ -2,44 +2,46 @@ import aiohttp
 import asyncio
 import argparse
 import json
-import csv
+import docx
 
 
-def write_lab_data_to_csv(labs_data, file_name):
+async def write_lab_data_to_word(labs_data, file_name):
+    document = docx.Document()
+
     if isinstance(labs_data, dict):
         labs_data = [labs_data]
 
-    with open(f'{file_name}.csv', mode='w', newline='', encoding='utf-8') as file:
-        fieldnames = ['Название'] + [lab['name'] for lab in labs_data]
+    table = document.add_table(rows=1, cols=len(labs_data) + 1)
+    header_cells = table.rows[0].cells
+    header_cells[0].text = 'Название'
+    for i, lab in enumerate(labs_data):
+        header_cells[i + 1].text = lab['name']
 
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
+    all_students = set()
+    for lab in labs_data:
+        students = lab.get('students', '').split(',')
+        if students and any(name.strip() for name in students):
+            all_students.update(students)
 
-        writer.writeheader()
+    for key in ['Дедлайн', 'Описание'] + list(all_students):
+        row_cells = table.add_row().cells
+        row_cells[0].text = key
+        for i, lab in enumerate(labs_data):
+            if key == 'Дедлайн':
+                row_cells[i + 1].text = lab.get('deadline', '')
+            elif key == 'Описание':
+                row_cells[i + 1].text = lab.get('description', '')
+            else:
+                row_cells[i + 1].text = '+' if key in lab.get('students', '').split(',') else ''
 
-        all_students = set()
-        for lab in labs_data:
-            students = lab.get('students', '').split(',')
-            if students and any(name.strip() for name in students):
-                all_students.update(students)
-
-        for key in ['Дедлайн', 'Описание'] + list(all_students):
-            row_data = {'Название': key}
-            for lab in labs_data:
-                if key == 'Дедлайн':
-                    row_data[lab['name']] = lab.get('deadline', '')
-                elif key == 'Описание':
-                    row_data[lab['name']] = lab.get('description', '')
-                else:
-                    row_data[lab['name']] = '+' if key in lab.get('students', '').split(',') else ''
-
-            writer.writerow(row_data)
+    document.save(f'{file_name}.docx')
 
 
 async def print_response(response):
     print(f"Status: {response.status}")
     location_header = response.headers.get('Location', '')
-    if len(location_header):
-        print(f'URL:{location_header}')
+    if location_header:
+        print(f'URL: {location_header}')
     text = await response.text()
     if text:
         print(text)
@@ -52,17 +54,17 @@ async def add_lab(url, lab, deadline, description):
             await print_response(response)
 
 
-async def update_lab(url, lab, deadline, description, students):
-    async with aiohttp.ClientSession() as session:
-        request = {}
-        if deadline is not None:
-            request['deadline'] = deadline
-        if description is not None:
-            request['description'] = description
-        if students is not None:
-            request['students'] = students
+async def update_lab(url, lab, deadline=None, description=None, students=None):
+    request_data = {}
+    if deadline is not None:
+        request_data['deadline'] = deadline
+    if description is not None:
+        request_data['description'] = description
+    if students is not None:
+        request_data['students'] = students
 
-        async with session.patch(url=f"{url}/labs/{lab}", data=json.dumps(request)) as response:
+    async with aiohttp.ClientSession() as session:
+        async with session.patch(url=f"{url}/labs/{lab}", data=json.dumps(request_data)) as response:
             await print_response(response)
 
 
@@ -79,9 +81,9 @@ async def get_lab(url, name):
             if response.status == 200:
                 try:
                     data = await response.json()
-                    write_lab_data_to_csv(data, name)
-                except Exception as ex:
-                    print(ex)
+                    await write_lab_data_to_word(data, name)
+                except json.JSONDecodeError as ex:
+                    print(f"Error decoding JSON: {ex}")
 
 
 async def get_all(url):
@@ -90,24 +92,26 @@ async def get_all(url):
             await print_response(response)
             try:
                 data = await response.json()
-                write_lab_data_to_csv(data, 'all_info_labs')
-            except Exception as ex:
-                print(ex)
+                await write_lab_data_to_word(data, 'all_info_labs')
+            except json.JSONDecodeError as ex:
+                print(f"Error decoding JSON: {ex}")
 
 
 async def process_cli_args(cli_args):
     url = 'http://0.0.0.0:8080'
 
-    if cli_args.add and not cli_args.deadline:
-        print("Deadline is required (--deadline)")
-    elif cli_args.add and cli_args.deadline:
-        description = cli_args.description if hasattr(cli_args, 'description') else None
-        await add_lab(url, cli_args.add, cli_args.deadline, description)
+    if cli_args.add:
+        if not cli_args.deadline:
+            print("Deadline is required (--deadline)")
+        else:
+            description = getattr(cli_args, 'description', None)
+            await add_lab(url, cli_args.add, cli_args.deadline, description)
 
-    if cli_args.update and not (cli_args.deadline or cli_args.description or cli_args.students):
-        print("Required --deadline --description or --students")
-    elif cli_args.update:
-        await update_lab(url, cli_args.update, cli_args.deadline, cli_args.description, cli_args.students)
+    if cli_args.update:
+        if not (cli_args.deadline or cli_args.description or cli_args.students):
+            print("Required --deadline --description or --students")
+        else:
+            await update_lab(url, cli_args.update, cli_args.deadline, cli_args.description, cli_args.students)
 
     if cli_args.delete:
         await delete_lab(url, cli_args.delete)
@@ -118,24 +122,24 @@ async def process_cli_args(cli_args):
     if cli_args.get_all:
         await get_all(url)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process command line arguments for lab management.")
 
-parser = argparse.ArgumentParser()
+    parser.add_argument('--deadline', type=str, help="Deadline of laboratory - day.month.year")
+    parser.add_argument('--description', type=str, help="Description of laboratory")
+    parser.add_argument('--students', type=str, help="Students who completed the laboratory")
 
-parser.add_argument('--deadline', type=str, help="Deadline of laboratory - day.month.year")
-parser.add_argument('--description', type=str, help="Description of laboratory")
-parser.add_argument('--students', type=str, help="Students who completed the laboratory")
+    group = parser.add_mutually_exclusive_group(required=True)
 
-group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--add', type=str, help="adding new laboratory")
 
-group.add_argument('--add', type=str, help="adding laboratory")
+    group.add_argument('--update', type=str, help="updating fields")
 
-group.add_argument('--update', type=str, help="updating fields")
+    group.add_argument('--delete', type=str, help="delete laboratory")
 
-group.add_argument('--delete', type=str, help="delete laboratory")
+    group.add_argument('--get_lab', type=str, help="getting data of laboratory")
 
-group.add_argument('--get_lab', type=str, help="getting data of laboratory")
+    group.add_argument('--get_all', action='store_true', help="getting all data of laboratory")
 
-group.add_argument('--get_all', action='store_true', help="getting all data of laboratory")
-
-args = parser.parse_args()
-asyncio.run(process_cli_args(args))
+    args = parser.parse_args()
+    asyncio.run(process_cli_args(args))
